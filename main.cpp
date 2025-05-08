1c9c9f9bedfb95862089e64abd9143917400cde8
Ni7aspinner <167442317+Ni7aspinner@users.noreply.github.com>
2025-05-08 21:56:51 +0300

    new backgrounds + randomized

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QScreen>
 #include <QFile>
 #include <QDir>
+#include <random>
 
 int main(int argc, char *argv[])
 {
@@ -30,10 +31,17 @@ int main(int argc, char *argv[])
     w->setGeometry(QGuiApplication::screens()[0]->geometry());
     w->setWindowTitle("MINESWEEPER 2");
 
-    QPixmap bg(":/images/backgrounds/background.png");
+    /*QPixmap bg(":/images/backgrounds/background.png");
     bg = bg.scaled(w->size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
     QPalette palette;
     palette.setBrush(QPalette::Window, bg);
+    */
+    std::default_random_engine engine(std::chrono::system_clock::now().time_since_epoch().count());
+    std::uniform_int_distribution<int> distX(1, 8);
+    QString bgPath = ":/images/backgrounds/Bg" +QString::number(distX(engine))+".PNG";
+    QPixmap bg(bgPath);
+    QPalette palette;
+    palette.setBrush(QPalette::Window, QBrush(bg));
     w->setAutoFillBackground(true);
     w->setPalette(palette);
 
